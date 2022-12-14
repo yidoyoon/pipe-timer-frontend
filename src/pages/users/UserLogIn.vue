7dd22ca4707bf52b3216c94fe4a96076bacb4d3d
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-12-14 22:58:53 +0900

    feat: add login input validation

---
+++ src/pages/users/UserLogIn.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="q-pa-md absolute-center" style="min-width: 400px">
+    <div class="text-h4 q-mb-lg">Login</div>
+    <q-form @submit.prevent="onSubmit" class="q-gutter-sm" autocomplete="off">
+      <q-input
+        filled
+        v-model="email"
+        label="Email"
+        placeholder="account@example.com"
+        :error-message="emailError"
+        :error="!!emailError"
+      />
+      <q-input
+        filled
+        v-model="password"
+        label="Password"
+        :type="isPwd ? 'password' : 'text'"
+        placeholder="8 - 128 characters"
+        :error-message="passwordError"
+        :error="!!passwordError"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+
+      <div class="row">
+        <q-btn label="SIGN IN" type="submit" color="primary" />
+        <div class="q-space"></div>
+        <q-btn label="CANCEL" to="/" color="primary" flat class="q-ml-sm" />
+      </div>
+      <div class="text-body2">
+        계정이 없다면 회원가입 해주세요.
+        <a href="/signup">회원가입</a>
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeUpdate, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useQuasar } from 'quasar';
+import { useMutation, useQuery, useQueryClient } from 'vue-query';
+import { useField, useForm } from 'vee-validate';
+import { useAuthStore } from 'src/router/authStore';
+import { toFormValidator } from '@vee-validate/zod';
+import { getMeFn, loginUserFn } from 'src/api/authApi';
+import { ILoginInput } from 'src/api/userTypes';
+import * as zod from 'zod';
+
+const router = useRouter();
+
+const authStore = useAuthStore();
+
+const $q = useQuasar();
+
+const loginSchema = toFormValidator(
+  zod.object({
+    email: zod
+      .string()
+      .min(1, '이메일을 입력해주세요.')
+      .email('유효하지 않은 이메일 형식입니다.'),
+    password: zod.string().min(1, '비밀번호를 입력해주세요.'),
+  })
+);
+
+const { handleSubmit, resetForm } = useForm({
+  validationSchema: loginSchema,
+});
+
+const { value: email, errorMessage: emailError } = useField('email');
+const { value: password, errorMessage: passwordError } = useField('password');
+
+const isPwd = ref(true);
+
+const authResult = useQuery('api', () => getMeFn(), {
+  enabled: false,
+  retry: 1,
+});
+
+const queryClient = useQueryClient();
+
+const { isLoading, mutate } = useMutation(
+  (credentials: ILoginInput) => loginUserFn(credentials),
+  {
+    onError: (error) => {
+      const errorMsg = (<any>error).response.data.error;
+      const responseMsg = (<any>error).response.data.message;
+      if (Array.isArray(errorMsg)) {
+        errorMsg.forEach((el: any) => {
+          $q.notify({
+            type: 'negative',
+            message: el.message,
+            icon: 'warning',
+          });
+        });
+      } else {
+        $q.notify({
+          type: 'negative',
+          message: responseMsg,
+          icon: 'warning',
+        });
+      }
+    },
+    onSuccess: () => {
+      queryClient.refetchQueries('api');
+      $q.notify({
+        type: 'positive',
+        message: '로그인에 성공했습니다.',
+        icon: 'warning',
+      });
+      router.push({ name: 'index' });
+    },
+  }
+);
+
+const onSubmit = handleSubmit((values) => {
+  mutate({
+    email: values.email,
+    password: values.password,
+  });
+  resetForm();
+});
+
+onBeforeUpdate(() => {
+  if (authResult.isSuccess.value) {
+    const authUser = Object.assign({}, authResult.data.value?.data.user);
+    authStore.setAuthUser(authUser);
+  }
+});
+</script>
+
+<style lang="scss">
+.text-caption {
+  color: $negative;
+}
+</style>
