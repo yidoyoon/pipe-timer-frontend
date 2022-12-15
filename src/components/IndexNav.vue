398e96e0cf6b22b68087b9229ffd1eb138384305
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-12-15 21:00:32 +0900

    reformat: reformat navigation header

--- src/components/IndexNav.vue
+++ src/components/IndexNav.vue
@@ -9,26 +9,65 @@
   <!--  Spacing-->
   <div class="q-space"></div>
 
-  <!--        Log in-->
+  <!--  Log in-->
   <q-btn v-if="!isLoggedIn" color="secondary" label="LOG IN" to="/login" />
+
+  <!--  Profile-->
+  <q-btn v-if="isLoggedIn" color="info" label="PROFILE" to="/profile" />
 
   <!--  Logout-->
   <q-btn
-    class="q-ml-lg"
+    class="q-ml-sm"
     v-if="isLoggedIn"
     color="negative"
     label="LOGOUT"
-    to="/"
+    @click="handleLogout"
   />
 </template>
 
 <script setup lang="ts">
 import { createApp } from 'vue';
+import { useAuthStore } from 'src/router/authStore';
+import { useMutation } from 'vue-query';
+import { logoutUserFn } from 'src/api/authApi';
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
 
 const app = createApp({});
 app.component('IndexNav');
 
-const isLoggedIn = false;
+const authStore = useAuthStore();
+const isLoggedIn = authStore.authUser;
+// const isLoggedIn = true;
+
+const $q = useQuasar();
+const router = useRouter();
+
+const { mutate: logoutUser } = useMutation(() => logoutUserFn(), {
+  onSuccess: () => {
+    authStore.setAuthUser(null);
+    router.push({ name: 'index' });
+  },
+  onError: (error) => {
+    if (Array.isArray((<any>error).response.data.error)) {
+      (<any>error).response.data.error.forEach((el: any) =>
+        $q.notify({
+          type: 'warning',
+          message: el.message,
+        })
+      );
+    } else {
+      $q.notify({
+        type: 'negative',
+        message: (<any>error).response.data.message,
+      });
+    }
+  },
+});
+
+const handleLogout = () => {
+  logoutUser();
+};
 
 const navLinks = [
   {
