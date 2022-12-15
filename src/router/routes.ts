b07667575a0e1eb09944c6ebe81ab88b17083c4b
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-12-15 20:53:42 +0900

    fix: fix routing

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,19 +5,42 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue'), name: 'index' },
-      { path: 'guide', component: () => import('pages/navigation/AppGuide.vue'), name: 'guide'},
-      { path: 'dashboard', component: () => import('pages/navigation/HistoryDashboard.vue'), name: 'dashboard' },
-      { path: 'settings', component: () => import('pages/navigation/AppSettings.vue'), name: 'settings' },
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue'),
+        name: 'index',
+      },
+      {
+        path: 'guide',
+        component: () => import('pages/navigation/AppGuide.vue'),
+        name: 'guide',
+      },
+      {
+        path: 'dashboard',
+        component: () => import('pages/navigation/HistoryDashboard.vue'),
+        name: 'dashboard',
+      },
+      {
+        path: 'settings',
+        component: () => import('pages/navigation/AppSettings.vue'),
+        name: 'settings',
+      },
+      {
+        path: 'login',
+        component: () => import('pages/users/UserLogIn.vue'),
+        name: 'login',
+      },
+      {
+        path: 'signup',
+        component: () => import('pages/users/UserSignUp.vue'),
+        name: 'signup',
+      },
     ],
   },
   {
     path: '/users',
-    // component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: 'login', component: () => import('pages/users/UserLogIn.vue'), name: 'login' },
-      { path: 'signup', component: () => import('pages/users/UserSignUp.vue'), name: 'signup' },
-    ],
+    component: () => import('layouts/MainLayout.vue'),
+    children: [],
   },
   // Always leave this as last one,
   // but you can also remove it
