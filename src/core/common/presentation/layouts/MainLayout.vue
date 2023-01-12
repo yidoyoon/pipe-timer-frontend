6cadce7af2f703cf3c9f052e5978aa5e16ecd2a1
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-12 22:06:56 +0900

    Remove main layout footer

--- src/core/common/presentation/layouts/MainLayout.vue
+++ src/core/common/presentation/layouts/MainLayout.vue
@@ -14,21 +14,6 @@
       </keep-alive>
     </q-page-container>
 
-    <q-footer class="bg-grey-8 text-white">
-      <q-toolbar shrink>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-        </q-toolbar-title>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-          aliquid aspernatur atque beatae doloremque eius, hic id illum iusto,
-          modi molestias provident quos sapiente sit temporibus veniam vero
-          voluptas, voluptatibus.
-        </div>
-      </q-toolbar>
-    </q-footer>
   </q-layout>
 </template>
 
