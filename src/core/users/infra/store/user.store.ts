c5204a8e69fa8cb7ec95b4a12276250058b38e0f
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-19 15:15:26 +0900

    Add persist option to User storage

--- src/core/users/infra/store/user.store.ts
+++ src/core/users/infra/store/user.store.ts
@@ -1,11 +1,29 @@
-import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
+import { LocalStorage } from 'quasar';
 import { IUser } from 'src/type-defs/userTypes';
 
+interface UserState {
+  user: IUser | null;
+}
+
 export const useUserStore = defineStore('userStore', {
-  state: () => ({
-    user: useLocalStorage<IUser | null>('user', null),
+  state: (): UserState => ({
+    user: null,
   }),
+  persist: {
+    key: 'user',
+    storage: {
+      getItem: (key) => {
+        return LocalStorage.getItem(key);
+      },
+      setItem: (key, value) => {
+        return LocalStorage.set(key, value);
+      },
+    },
+    beforeRestore: (ctx) => {
+      console.log(`about to restore '${ctx.store.$id}'`);
+    },
+  },
   getters: {},
   actions: {
     setUser(user: IUser | null) {
