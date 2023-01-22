d1e25a43edd9e0dca24e1d34c9468d56c60645ca
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-22 23:03:08 +0900

    Cleanup codes

--- src/core/timefrag/presentation/components/TimefragCore.vue
+++ src/core/timefrag/presentation/components/TimefragCore.vue
@@ -1,92 +1,16 @@
-<script setup lang="ts">
-import { storeToRefs } from 'pinia';
-import { ITimefrag } from 'src/core/timefrag/domain/timefrag';
-import { useTimefragStore } from 'src/core/timefrag/infra/store/timefrag.store';
-import { ref } from 'vue';
-import { useQuasar } from 'quasar';
-
-const props = defineProps<{ timefrag: ITimefrag }>();
-
-const emit = defineEmits<{
-  (e: 'upsert', data: ITimefrag): void;
-  (e: 'remove', id: string): void;
-}>();
-
-const timefragStore = useTimefragStore();
-const { isLoading } = storeToRefs(timefragStore);
-
-const $q = useQuasar();
-
-const _name = ref(props.timefrag._name);
-const _duration = ref(props.timefrag._duration);
-const _count = ref(props.timefrag._count);
-const _color = ref(props.timefrag._color);
-const _isEditing = ref(props.timefrag._isEditing);
-
-// TODO: 아래 코드처럼 emit 사용하여 데이터 전송하지않고 컴포넌트에서 처리하도록 다른 함수들 수정
-const update = () => {
-  const newFrag = {
-    ...props.timefrag,
-    _name: _name.value,
-    _duration: _duration.value,
-    _color: _color.value,
-    _isEditing: !_isEditing.value,
-  };
-  timefragStore.edit(newFrag);
-};
-
-const upsert = () => {
-  emit('upsert', {
-    _id: props.timefrag._id,
-    _name: _name.value,
-    _duration: _duration.value,
-    _count: _count.value,
-    _color: _color.value,
-    _isEditing: _isEditing.value,
-  });
-  update();
-};
-
-const remove = () => {
-  $q.notify({
-    progress: true,
-    message: '해당 스택을 삭제할까요?',
-    color: 'indigo-5',
-    multiLine: true,
-    actions: [
-      {
-        label: '확인',
-        color: 'negative',
-        handler: () => {
-          isLoading.value = false;
-          emit('remove', props.timefrag._id);
-        },
-      },
-      { label: '취소', color: 'white' },
-    ],
-  });
-};
-
-const cancel = () => {
-  _name.value = props.timefrag._name;
-  _duration.value = props.timefrag._duration;
-  _color.value = props.timefrag._color;
-
-  update();
-};
-</script>
-
 <!--TODO: Font 컬러 설정 추가, 입력값 필터링 추가-->
 <template>
   <q-card
-    class="my-card text-white overflow-hidden"
-    style="background: radial-gradient(circle, #4b6e8c 0%, #4b6e8c 100%)"
+    class="my-card text-white cursor-pointer"
+    style="background: black"
+    @click="toStack"
+    v-ripple
   >
-    <q-inner-loading :showing="isLoading" style="z-index: 1">
+    <q-inner-loading :showing="isLoadingFrags" style="z-index: 1">
       <q-spinner size="50px" color="primary" />
     </q-inner-loading>
 
-<!--    // TODO: 수정 표시가 아닌, 더블 클릭으로 수정하도록 변경-->
+    <!--    // TODO: 수정 표시가 아닌, 더블 클릭으로 수정하도록 변경-->
     <q-card-section>
       <div class="row justify-between">
         <div class="row items-center q-gutter-x-xs col-6">
@@ -171,4 +95,84 @@ const cancel = () => {
   </q-card>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useStacksStore } from 'src/core/stack/infra/store/stacks.store';
+import { ITimefrag } from 'src/core/timefrag/domain/timefrag';
+import { useTimefragStore } from 'src/core/timefrag/infra/store/timefrag.store';
+import { ref } from 'vue';
+import { useQuasar } from 'quasar';
+
+const timefragStore = useTimefragStore();
+const stacksStore = useStacksStore();
+
+const { isLoadingFrags } = storeToRefs(timefragStore);
+
+const $q = useQuasar();
+
+const props = defineProps<{ timefrag: ITimefrag }>();
+const emit = defineEmits<{
+  (e: 'upsert', data: ITimefrag): void;
+  (e: 'remove', id: string): void;
+}>();
+
+const _name = ref(props.timefrag._name);
+const _duration = ref(props.timefrag._duration);
+const _count = ref(props.timefrag._count);
+const _color = ref(props.timefrag._color);
+const _isEditing = ref(props.timefrag._isEditing);
+
+// TODO: 아래 코드처럼 emit 사용하여 데이터 전송하지않고 컴포넌트에서 처리하도록 다른 함수들 수정
+const update = () => {
+  const newFrag = {
+    ...props.timefrag,
+    _name: _name.value,
+    _duration: _duration.value,
+    _color: _color.value,
+    _isEditing: !_isEditing.value,
+  };
+  timefragStore.edit(newFrag);
+};
+
+const upsert = () => {
+  emit('upsert', {
+    _id: props.timefrag._id,
+    _name: _name.value,
+    _duration: _duration.value,
+    _count: _count.value,
+    _color: _color.value,
+    _isEditing: _isEditing.value,
+  });
+  update();
+};
+
+const remove = () => {
+  $q.notify({
+    progress: true,
+    message: '해당 스택을 삭제할까요?',
+    color: 'indigo-5',
+    multiLine: true,
+    actions: [
+      {
+        label: '확인',
+        color: 'negative',
+        handler: () => {
+          isLoadingFrags.value = false;
+          emit('remove', props.timefrag._id);
+        },
+      },
+      { label: '취소', color: 'white' },
+    ],
+  });
+};
+
+const cancel = () => {
+  _name.value = props.timefrag._name;
+  _duration.value = props.timefrag._duration;
+  _color.value = props.timefrag._color;
+
+  update();
+};
+
+const toStack = () => {};
+</script>
