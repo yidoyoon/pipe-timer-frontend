6ac01e90df57410c44d44e30608cf5026bc5e60e
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-27 21:32:58 +0900

    Refactor components into 'builder' and 'stack'
    Refactor components into 'builder' and 'stack' for improved management

--- src/core/builder/presentation/components/BuilderCore.vue
+++ src/core/builder/presentation/components/BuilderCore.vue
@@ -6,40 +6,28 @@
   <!--    separator-->
   <!--    v-slot="{ item, index }"-->
   <!--  >-->
-  <q-card
-    class="row my-card text-black"
-    style="border: 0.3em solid dodgerblue; width: 100vw; height: 100vh;"
-  >
-    <q-inner-loading :showing="isLoadingStacks" style="z-index: 1">
-      <q-spinner size="50px" color="primary" />
-    </q-inner-loading>
+  <q-card class="fit flat no-shadow my-card cursor-pointer">
+    <!--    <q-inner-loading :showing="isLoadingStacks" style="z-index: 1">-->
+    <!--      <q-spinner size="50px" color="primary" />-->
+    <!--    </q-inner-loading>-->
 
-    <!--    // TODO: 수정 표시가 아닌, 더블 클릭으로 수정하도록 변경-->
-    <q-card-section>
-      <div class="row">
-        <p class="text-h6">Stack name: {{ stackName }}</p>
-        <q-input
-          class="text-h6"
-          v-model="stackName"
-          :readonly="!isEditing"
-          dense
-          :borderless="!isEditing"
-          :input-style="{
-            color: 'black',
-            display: 'none',
-            width: '80vw',
-          }"
-        />
+    <q-card-section class="row" style="height: 29.5vh">
+      <div class="text-h6">
+        Stack name: {{ stackName }} | Total duration: {{ getTotalDuration }}
       </div>
+      <q-space class="flex-break"></q-space>
       <!--      Inner Stack Frags -->
-      <div class="row no-wrap" style="height: 7rem; white-space: nowrap">
+      <div
+        class="row no-wrap flat justify-between"
+        style="height: 7rem; white-space: nowrap"
+      >
         <div
-          v-for="(f, idx) in fragsInStack"
+          v-for="(f, idx) in timersInStack"
           :key="f"
-          class="col-2 no-padding row no-wrap justify-around"
+          class="q-pa-none row no-wrap justify-around"
         >
           <q-card
-            class="inner-my-card overflow-hidden text-white cursor-pointer"
+            class="inner-my-card text-white cursor-pointer flat justify-between"
             style="background: black; display: inline-block"
           >
             <q-card-section class="q-img-container">
@@ -53,7 +41,7 @@
           </q-card>
           <div class="row items-center">
             <q-icon
-              v-if="idx !== fragsInStack.length - 1"
+              v-if="idx !== timersInStack.length - 1"
               name="arrow_right"
               style="font-size: 4rem; color: grey"
             ></q-icon>
@@ -66,19 +54,20 @@
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
-import { useQuasar } from 'quasar';
-import { IStacks } from 'src/core/stack/domain/stacks.model';
-import { useStacksStore } from 'src/core/stack/infra/store/stacks.store';
+import { useQuasar }     from 'quasar';
+import { IStack }        from 'src/core/stack/domain/stack.model';
+import { useStackStore } from 'src/core/stack/infra/store/stack.store.ts';
 import { ref } from 'vue';
 
 const $q = useQuasar();
 
-const stackStore = useStacksStore();
-const { isLoadingStacks, fragsInStack } = storeToRefs(stackStore);
+const stackStore = useStackStore();
+const { isLoadingStacks, timersInStack, getTotalDuration, listStacks } =
+  storeToRefs(stackStore);
 
-const props = defineProps<{ stacks: IStacks }>();
+const props = defineProps<{ stacks: IStack }>();
 const emit = defineEmits<{
-  (e: 'upsert', data: IStacks): void;
+  (e: 'upsert', data: IStack): void;
   (e: 'remove', id: string): void;
 }>();
 
@@ -86,17 +75,15 @@ const emit = defineEmits<{
 const stackName = ref(props.stacks._name);
 const count = ref(props.stacks._count);
 const isEditing = ref(props.stacks._isEditing);
+const stackData = ref(props.stacks._data);
 
-// Frag properties
-// const fragName
-// const fragDurtaion
-// const fragColor
-// const fragCount
+const totalDuration = ref(0);
 
 const update = () => {
   const newFrag = {
     ...props.stacks,
     _name: stackName.value,
+    _data: stackData.value,
     _isEditing: !isEditing.value,
   };
   stackStore.edit(newFrag);
@@ -107,6 +94,7 @@ const upsert = () => {
     _id: props.stacks._id,
     _name: stackName.value,
     _count: count.value,
+    _data: stackData.value,
     _isEditing: isEditing.value,
   });
   update();
@@ -141,7 +129,7 @@ const cancel = () => {
 
 <style lang="scss" scoped>
 .inner-my-card {
-  width: 85%;
+  width: 15rem;
 }
 
 .q-img-container {
@@ -151,5 +139,19 @@ const cancel = () => {
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
+}
+
+.flex-break {
+  flex: 1 0 100% !important;
+}
+.row {
+  .flex-break {
+    height: 0 !important;
+  }
+}
+.column {
+  .flex-break {
+    width: 0 !important;
+  }
 }
 </style>
