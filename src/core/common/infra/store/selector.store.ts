f810caa891ffc5e0de2137f294d751b8c875a588
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-01-27 21:41:22 +0900

    Add editor module, button footers
    Add editor module, button footers for managing stack, builder, timer

---
+++ src/core/common/infra/store/selector.store.ts
@@ -0,0 +1,55 @@
+import { defineStore } from 'pinia';
+
+export interface IEditNow {
+  onStack: boolean;
+  onBuilder: boolean;
+  onTimer: boolean;
+}
+
+export const useSelectorStore = defineStore('SelectorStore', {
+  state: (): IEditNow => {
+    return {
+      onStack  : false,
+      onBuilder: false,
+      onTimer  : false,
+    };
+  },
+
+  getters: {
+    editNow(): string | null {
+      if (this.onStack && !this.onTimer && !this.onBuilder) {
+        return 'onStack';
+      } else if (this.onBuilder && !this.onStack && !this.onTimer) {
+        return 'onBuilder';
+      } else if (this.onTimer && !this.onStack && !this.onBuilder) {
+        return 'onTimer';
+      } else if (!this.onStack && !this.onBuilder && !this.onTimer) {
+        return null;
+      }
+      return null;
+    },
+  },
+
+  actions: {
+    selectStack() {
+      this.onBuilder = false;
+      this.onTimer = false;
+      this.onStack = !this.onStack;
+    },
+    selectBuilder() {
+      this.onStack = false;
+      this.onTimer = false;
+      this.onBuilder = !this.onBuilder;
+    },
+    selectTimer() {
+      this.onStack = false;
+      this.onBuilder = false;
+      this.onTimer = !this.onTimer;
+    },
+    selectNone() {
+      this.onStack = false;
+      this.onBuilder = false;
+      this.onTimer = false;
+    },
+  },
+});
---
+++ src/core/common/presentation/components/EditorMain/StackFooter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row q-pa-md justify-between">
+    <div>
+      <q-btn @click="toBuilderBtn" color="negative" label="Remove" />
+    </div>
+    <div>
+      <q-btn outline @click="toBuilderBtn" label="Move to Builder" />
+      <q-btn
+        outline
+        @click="toBuilderBtn"
+        label="Rename Stack"
+        class="q-mx-md"
+      />
+    </div>
+    <div>
+      <q-btn
+        @click="saveEdit"
+        color="blue"
+        label="Save Stack"
+        :disable="!canSaveTimers"
+        class="q-mx-md"
+      />
+      <q-btn
+        @click="cancelEdit"
+        color="white"
+        text-color="black"
+        label="cancel"
+      />
+    </div>
+    <q-tooltip v-if="!canSaveTimers" class="text-body2">
+      진행중인 설정이 남아있거나, 데이터 로딩이 완료되지 않았습니다.
+    </q-tooltip>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useQuasar }   from 'quasar';
+import {useStackStore} from 'src/core/stack/infra/store/stack.store.ts';
+
+const $q = useQuasar();
+const stackStore = useStackStore();
+
+const toBuilderBtn = () => {};
+</script>
