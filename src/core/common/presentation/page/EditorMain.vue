4b93e867e57da430b1fd9d176af4561e3150698b
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-03 15:34:17 +0900

    Implement dynamic Footer based on current state

--- src/core/common/presentation/page/EditorMain.vue
+++ src/core/common/presentation/page/EditorMain.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="fit row wrap justify-between q-pa-md">
-    <!--    TODO: 현재 작업중인 영역에 border 표시 되도록 구현-->
     <div class="col-9 q-pa-sm">
       <StackMain class="area"></StackMain>
       <q-space style="height: 1rem"></q-space>
-      <BuilderMain class="area"></BuilderMain>
+      <BuilderMain
+        class="area"
+        @click="selectBuilder"
+        :style="isEditBuilder ? active : {}"
+      ></BuilderMain>
     </div>
     <div class="col-3 q-pa-sm">
-      <TimerMain class="area"></TimerMain>
+      <TimerMain class="area" @click="selectTimer"></TimerMain>
     </div>
   </div>
   <q-footer>
     <div v-if="editNow === ''">
       <DefaultFooter></DefaultFooter>
     </div>
-    <!--    <div v-else-if="editNow === 'stack'">-->
-    <!--      <StackFooter></StackFooter>-->
-    <!--    </div>-->
-    <!--    <div v-else-if="editNow === 'builder'">-->
-    <!--      <BuilderFooter></BuilderFooter>-->
-    <!--    </div>-->
-    <!--    <div v-else-if="editNow === 'timer'">-->
-    <!--      <TimerFooter></TimerFooter>-->
-    <!--    </div>-->
+    <div v-else-if="isEditBuilder ? editNow === 'builder' : (editNow = '')">
+      <BuilderFooter></BuilderFooter>
+    </div>
+    <div v-else-if="editNow === 'stack'">
+      <StackFooter></StackFooter>
+    </div>
+    <div v-else-if="editNow === 'timer'">
+      <TimerFooter></TimerFooter>
+    </div>
   </q-footer>
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
+import { useBuilderStore } from 'src/core/builder/infra/store/builder.store';
 import { useSelectorStore } from 'src/core/common/infra/store/selector.store';
 import TimerMain from 'src/core/timer/presentation/TimerMain.vue';
 import BuilderMain from 'src/core/builder/presentation/BuilderMain.vue';
 import StackMain from 'src/core/stack/presentation/StackMain.vue';
-import { onMounted, ref } from 'vue';
 
 import DefaultFooter from 'src/core/common/presentation/components/EditorMain/DefaultFooter.vue';
 import BuilderFooter from 'src/core/common/presentation/components/EditorMain/BuilderFooter.vue';
@@ -40,9 +43,9 @@ import TimerFooter from 'src/core/common/presentation/components/EditorMain/Time
 import StackFooter from 'src/core/common/presentation/components/EditorMain/StackFooter.vue';
 
 const selectorStore = useSelectorStore();
-const { onStack, onBuilder, onTimer } = storeToRefs(selectorStore);
+const { editNow } = storeToRefs(selectorStore);
 
-const editNow = ref('');
+const { isEditBuilder, stackInBuilder } = storeToRefs(useBuilderStore());
 
 const selectStack = () => {
   selectorStore.selectStack();
@@ -57,10 +60,16 @@ const selectTimer = () => {
   editNow.value = 'timer';
 };
 
-// TODO: 선택영역 외 클릭하면 선택이 풀리도록 구현
-// onMounted(() = {
-//
-// })
+const active = {
+  borderColor: 'skyblue',
+  borderWidth: '0.3rem',
+};
+
+// watchEffect(async () => {
+//   if (isEditBuilder) {
+//     editNow.value = 'builder';
+//   }
+// });
 </script>
 
 <style scoped lang="scss">
