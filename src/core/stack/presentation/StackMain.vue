9ac6bba3a639ec01048fda7527b2d3ed908697e4
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-13 23:23:40 +0900

    Add builder to StackMain for visibility

--- src/core/stack/presentation/StackMain.vue
+++ src/core/stack/presentation/StackMain.vue
@@ -1,29 +1,51 @@
 <template>
+  <div v-if="stackInBuilder.stacksToFrag" class="highlight-stack">
+    <BuilderMain />
+  </div>
   <q-scroll-area
     :thumb-style="thumbStyle"
     :content-style="contentStyle"
     :content-active-style="contentActiveStyle"
-    style="height: 50vh"
+    style="height: 40vh"
   >
-    <div v-for="stack in listStacks" :key="stack" class="row justify-between">
-      <StackLoader :stack="stack" @remove="removeStacks" />
+    <div v-if="listStacks.length">
+      <div
+        v-for="stack in listStacks"
+        :key="stack"
+        class="row justify-start wrap q-pa-md"
+        style="border-bottom: solid 0.1rem grey"
+      >
+        <StackLoader :stack="stack" @remove="removeStacks" />
+      </div>
+    </div>
+    <div
+      v-if="isEmptyObj(stackInBuilder) && selectorStore.editNow !== 'builder'"
+      class="text-h6 text-weight-bold"
+    >
+      스택이 비어있습니다. 타이머를 활용해 스택을 생성해주세요.
     </div>
   </q-scroll-area>
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
+import { useBuilderStore } from 'src/core/builder/infra/store/builder.store';
+import BuilderMain from 'src/core/builder/presentation/BuilderMain.vue';
+import { useSelectorStore } from 'src/core/common/infra/store/selector.store';
 import { useStackStore } from 'src/core/stack/infra/store/stack.store';
 import StackLoader from 'src/core/stack/presentation/components/StackLoader.vue';
+import { isEmptyObj } from 'src/util/is-empty-object.util';
 
 const stackStore = useStackStore();
-
 const stacksStoreRefs = storeToRefs(stackStore);
+const selectorStore = useSelectorStore();
+const builderStore = useBuilderStore();
+const builderStoreRefs = storeToRefs(builderStore);
+const { stackInBuilder } = builderStoreRefs;
 
 stackStore.fetchAll();
 
 const { listStacks } = stacksStoreRefs;
-
 const removeStacks = (id: string) => {
   stackStore.remove(id);
 };
@@ -46,3 +68,13 @@ const thumbStyle = {
   opacity: '0.5',
 };
 </script>
+
+<style scoped lang="scss">
+.highlight-stack {
+  border: solid $fourth;
+}
+
+.flex-break {
+  flex: 1 0 100% !important;
+}
+</style>
