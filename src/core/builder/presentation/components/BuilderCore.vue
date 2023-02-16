dbc277e7edd81a115618d481d40dd9c23959dde5
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-16 20:43:08 +0900

    Add edit stack feature, fix timer format

--- src/core/builder/presentation/components/BuilderCore.vue
+++ src/core/builder/presentation/components/BuilderCore.vue
@@ -12,15 +12,31 @@
           class="row justify-between"
           style="background-color: #007777; color: white"
         >
-          <div class="q-mx-md">{{ props.stack.name }}</div>
-          <div class="q-mx-md">Total duration - {{ timeFormatter }}</div>
+          <div class="row justify-between">
+            <div class="q-ml-sm">{{ props.stack.name }}</div>
+            <div>
+              <q-btn
+                v-show="!isEmptyObj(stackInBuilder)"
+                icon="edit"
+                dense
+                flat
+                size="sm"
+                @click="editStackBtn"
+                style="position: relative; bottom: 2px"
+              />
+            </div>
+          </div>
+          <div class="q-mx-sm fixed-right">Total - {{ totalDuration }}</div>
+        </div>
+        <div
+          v-if="!stackInBuilder.stacksToFrag.length"
+          class="text-h6 text-weight-bold q-ml-sm"
+        >
+          우측 타이머 영역에서 추가하려는 타이머를 이곳에 드래그해주세요.
         </div>
       </div>
-      <div v-else class="text-h6 bold">
-        <b class="q-ml-sm">
-          우측의 타이머 리스트에서 타이머를 끌어와 주세요.
-        </b>
-      </div>
+
+      <q-space v-if="!stackInBuilder.stacksToFrag.length" class="q-my-lg" />
 
       <draggable
         v-if="props.stack.stacksToFrag !== undefined"
@@ -56,8 +72,11 @@
                 :style="colorExtractor(element.frag)"
               >
                 <q-card-section>
-                  <div>Name: {{ element.frag.name }}</div>
-                  <div>Duration: {{ element.frag.duration }}</div>
+                  <div>{{ element.frag.name }}</div>
+                  <div>
+                    <q-icon name="timer" />
+                    {{ timeFormatter(element.frag.duration) }}
+                  </div>
                 </q-card-section>
               </q-card>
               <div class="row items-center">
@@ -73,25 +92,61 @@
       </draggable>
     </q-item-section>
   </q-item>
+
+  <!--  Edit stack prompt-->
+  <q-dialog v-model="editStackPrompt" persistent>
+    <q-card style="min-width: 350px">
+      <q-card-section>
+        <div class="text-h6">새로운 스택 이름</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-input
+          dense
+          v-model="stackName"
+          autofocus
+          @keyup.enter.prevent="editStack"
+          @keyup.esc.prevent="editStackPrompt = false"
+        />
+      </q-card-section>
+
+      <q-card-actions align="right" class="text-primary">
+        <q-btn flat label="취소" v-close-popup @click="cancelEdit" />
+        <q-btn
+          flat
+          label="확인"
+          color="green"
+          v-close-popup
+          @click="editStack"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup lang="ts">
 import dayjs from 'dayjs';
 import { storeToRefs } from 'pinia';
+import { LocalStorage } from 'quasar';
 import { useBuilderStore } from 'src/core/builder/infra/store/builder.store';
 import { IStack } from 'src/core/stack/domain/stack.model';
 import { ITimer } from 'src/core/timer/domain/timer.model';
+import { isEmptyObj } from 'src/util/is-empty-object.util';
 import { computed, reactive, ref } from 'vue';
+import _ from 'lodash';
 import draggable from 'vuedraggable';
 
 const builderStore = useBuilderStore();
-const { getTotalDur, isEditBuilder } = storeToRefs(builderStore);
+const { getTotalDur, isEditBuilder, stackInBuilder } =
+  storeToRefs(builderStore);
+const editStackPrompt = ref(false);
 
 const props = defineProps<{ stack: IStack }>();
 const rBuilder = computed(() => {
   return reactive(props.stack.stacksToFrag);
 });
 
+const stackName = ref('');
 const drag = ref(false);
 
 const arrowDrawer = (index: number) => {
@@ -102,15 +157,33 @@ const removeDraggedItem = (e: any) => {
   builderStore.stackInBuilder.stacksToFrag.splice(e.oldIndex, 1);
 };
 
-const timeFormatter = computed(() => {
+const timeFormatter = (duration: string | number) => {
+  return computed(() => {
+    return dayjs.duration(+duration, 'seconds').format('HH:mm:ss');
+  });
+};
+
+const totalDuration = computed(() => {
   const formatted = dayjs
     .duration(getTotalDur.value, 'seconds')
     .format('HH:mm:ss');
   return formatted;
 });
 
-const activeBuilder = {
-  minHeight: '14rem',
+const editStackBtn = () => {
+  editStackPrompt.value = true;
+};
+
+const editStack = () => {
+  builderStore.stackInBuilder.name = stackName.value;
+  editStackPrompt.value = false;
+};
+
+const cancelEdit = () => {
+  const backup = LocalStorage.getItem('builder-backup') as IStack;
+  if (!!backup) {
+    builderStore.stackInBuilder = backup;
+  }
 };
 
 const dragOptions = {
