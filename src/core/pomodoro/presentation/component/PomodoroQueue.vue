1932ce1333089d4f41f499393e19cc4a8124feba
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-16 21:37:15 +0900

    Add vertical scroll to PomodoroQueue

--- src/core/pomodoro/presentation/component/PomodoroQueue.vue
+++ src/core/pomodoro/presentation/component/PomodoroQueue.vue
@@ -1,66 +1,75 @@
 <template>
   <div
     v-if="!isEmptyObj(timer) || !isEmptyObj(stack)"
-    class="row fit justify-center content-start"
+    class="row fit justify-center"
   >
-    <q-card class="no-shadow my-card" style="background: transparent">
-      <q-card-section class="q-py-md">
-        <!--        Stack-->
-        <div
-          v-if="pomodoroStore.mode === 'stack'"
-          class="row justify-between no-wrap"
-          style="height: 5rem; white-space: nowrap"
-        >
+    <q-scroll-area class="full-width" style="height: 15vh">
+      <q-card class="no-shadow my-card" style="background: transparent">
+        <q-card-section class="q-py-md relative-position" style="top: 4px">
+          <!--        Stack-->
           <div
-            v-for="(t, index) in stack.stacksToFrag"
-            :key="t.frag.fragId"
-            class="q-pa-none row no-wrap"
+            v-if="pomodoroStore.mode === 'stack'"
+            class="row justify-between no-wrap"
+            style="height: 5rem; white-space: nowrap"
           >
-            <q-card
-              class="inner-my-card text-white flat"
-              style="background: black; width: 12vw"
-              :style="
-                index === round ? highlightBorder(t.frag) : notCurrent(t.frag)
-              "
+            <div
+              v-for="(t, index) in stack.stacksToFrag"
+              :key="t.frag.fragId"
+              class="q-pa-none row no-wrap"
             >
-              <q-card-section v-show="'frag' in t" class="q-img-container">
-                <div>Name: {{ t.frag.name }}</div>
-                <div>Duration: {{ t.frag.duration }}<br /></div>
-              </q-card-section>
-            </q-card>
-            <div class="row items-center">
-              <q-icon
-                v-if="arrowDrawer(index)"
-                name="arrow_right"
-                style="font-size: 4rem; color: grey"
-              ></q-icon>
+              <q-card
+                class="inner-my-card text-white flat"
+                style="background: black; width: 12vw"
+                :style="
+                  index === round ? highlightBorder(t.frag) : notCurrent(t.frag)
+                "
+              >
+                <q-card-section v-show="'frag' in t" class="q-img-container">
+                  <div>{{ t.frag.name }}</div>
+                  <div>
+                    <q-icon name="timer" />
+                    {{ timeFormatter(t.frag.duration) }}<br />
+                  </div>
+                </q-card-section>
+              </q-card>
+              <div class="row items-center">
+                <q-icon
+                  v-if="arrowDrawer(index)"
+                  name="arrow_right"
+                  style="font-size: 4rem; color: grey"
+                ></q-icon>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!--        Timer-->
-        <div
-          v-else-if="pomodoroStore.mode === 'timer'"
-          class="row justify-between no-wrap"
-          style="height: 5rem; white-space: nowrap"
-        >
-          <div class="q-pa-none row no-wrap">
-            <q-card
-              class="inner-my-card text-white flat"
-              :style="highlightBorder(pomodoroStore.timer)"
-            >
-              <q-card-section class="q-img-container">
-                <div>Name: {{ pomodoroStore.timer.name }}</div>
-                <div>Duration: {{ pomodoroStore.timer.duration }}<br /></div>
-              </q-card-section>
-            </q-card>
+          <!--        Timer-->
+          <div
+            v-else-if="pomodoroStore.mode === 'timer'"
+            class="row justify-center no-wrap"
+            style="height: 5rem; white-space: nowrap"
+          >
+            <div class="q-pa-none row no-wrap">
+              <q-card
+                class="inner-my-card text-white flat"
+                style="width:24.8vh"
+                :style="highlightBorder(pomodoroStore.timer)"
+              >
+                <q-card-section class="q-img-container">
+                  <div>{{ pomodoroStore.timer.name }}</div>
+                  <div>
+                    <q-icon name="timer" />
+                    {{ timeFormatter(pomodoroStore.timer.duration) }}<br />
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
           </div>
-        </div>
-      </q-card-section>
-    </q-card>
+        </q-card-section>
+      </q-card>
+    </q-scroll-area>
   </div>
   <div v-else class="row justify-center items-center text-h6">
-      우측의 타이머 혹은 하단의 스택에서 설정하고자 하는 항목을 더블클릭 하세요.
+    우측의 타이머 혹은 하단의 스택에서 설정하고자 하는 항목을 더블클릭 하세요.
   </div>
 </template>
 
@@ -72,6 +81,7 @@ import { storeToRefs } from 'pinia';
 import { usePomodoroStore } from 'src/core/pomodoro/infra/store/pomodoro.store';
 import { ITimer } from 'src/core/timer/domain/timer.model';
 import { isEmptyObj } from 'src/util/is-empty-object.util';
+import { computed } from 'vue';
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
@@ -82,6 +92,12 @@ let { round } = storeToRefs(usePomodoroStore());
 
 const arrowDrawer = (index: number) => {
   return !!stack.value && index !== stack.value.stacksToFrag.length - 1;
+};
+
+const timeFormatter = (duration: string | number) => {
+  return computed(() => {
+    return dayjs.duration(+duration, 'seconds').format('HH:mm:ss');
+  });
 };
 
 const highlightBorder = (timer: ITimer) => {
