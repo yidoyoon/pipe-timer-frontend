50a2c2d78c0065e5e0e12bddc53b4df2cc29c27c
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-25 22:13:12 +0900

    Add LeftDrawer to replace q-tab

---
+++ src/core/common/presentation/components/LeftDrawer.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-drawer v-model="drawer" show-if-above :width="220" :breakpoint="400">
+    <q-scroll-area
+      style="
+        height: calc(100% - 33px);
+        margin-top: 33px;
+        border-right: 1px solid #ddd;
+      "
+    >
+      <q-list padding>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="inbox" />
+          </q-item-section>
+
+          <q-item-section> Inbox </q-item-section>
+        </q-item>
+
+        <q-item active clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="star" />
+          </q-item-section>
+
+          <q-item-section> Star </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="send" />
+          </q-item-section>
+
+          <q-item-section> Send </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="drafts" />
+          </q-item-section>
+
+          <q-item-section> Drafts </q-item-section>
+        </q-item>
+      </q-list>
+    </q-scroll-area>
+
+    <q-img
+      class="absolute-top"
+      src="https://img.yibyeongyong.com/public/dummy-logo-black.svg"
+      style="zoom: 11%; border-right: 1px solid #ddd;"
+    />
+  </q-drawer>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const drawer = ref(true);
+</script>
