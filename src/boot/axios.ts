6a2ce902d1f5398ce43273a00903dfb86e8327fe
Yi Byeongyong <ybyo@yibyeongyong.com>
2023-02-25 22:09:08 +0900

    Clean up CORS list for code readability

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -60,9 +60,10 @@ export default boot(({ app }) => {
   // TODO: Change axios instance name
   api.defaults.headers.common['Content-Type'] = 'application/json';
   api.defaults.headers.post['Access-Control-Allow-Origin'] = [
-    apiURL,
-    `${process.env.API_BASE_URL}`,
-    'localhost:3000',
+    'https://127.0.0.1:3000',
+    'https://127.0.0.1',
+    'https://localhost:3000',
+    'https://localhost',
   ];
 });
 
