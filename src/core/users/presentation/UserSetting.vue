926359e9bbeaa02d497653e837b4f7a74e5990b9
Yi doyoon <yidoyoon@yidoyoon.com>
2023-03-24 13:19:15 +0100

    PT-2 #comment Create a page for resetting account email, nickname, password, and deleting the account #resolve

---
+++ src/core/users/presentation/UserSetting.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="row justify-between">
+    <q-space class="col-2" />
+    <div class="row justify-between q-mt-xl col-8">
+      <div class="text-h4">Account Setting</div>
+    </div>
+    <q-space class="col-2" />
+  </div>
+
+  <q-separator spaced="lg" />
+
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7 q-gutter-lg">
+      <div class="row justify-start items-end">
+        <div class="text-h6 col-8">
+          Email
+          <q-field outlined style="width: 17rem" dense class="q-pt-xs">
+            <template v-slot:control>
+              <div class="self-center full-width no-outline" tabindex="0">
+                {{ userStore.user?.email }}
+              </div>
+            </template>
+          </q-field>
+        </div>
+        <q-btn outline class="col-4" text-color="black" label="Change Email" />
+      </div>
+
+      <div class="row justify-start items-end">
+        <div class="text-h6 col-8">
+          User name
+          <q-field outlined style="width: 17rem" dense class="q-pt-xs">
+            <template v-slot:control>
+              <div class="self-center full-width no-outline" tabindex="0">
+                {{ userStore.user?.userName }}
+              </div>
+            </template>
+          </q-field>
+        </div>
+        <q-btn
+          outline
+          class="col-4"
+          text-color="black"
+          label="Change Username"
+        />
+      </div>
+
+      <div class="row justify-start q-pt-lg items-end">
+        <div class="text-h6 col-8">
+          Password
+          <div class="text-caption q-pt-xs">
+            패스워드를 재설정합니다. 이메일이 필요합니다.
+          </div>
+        </div>
+        <q-btn
+          outline
+          class="col-4"
+          text-color="black"
+          label="Change Password"
+        />
+      </div>
+
+      <div class="row justify-between q-pt-lg items-end">
+        <div class="text-h6 col-8">
+          Delete Account
+          <div class="text-caption q-pt-xs">
+            계정을 삭제합니다. 모든 데이터가 제거됩니다.
+          </div>
+        </div>
+        <q-btn
+          class="col-4"
+          color="negative"
+          text-color="white"
+          label="Delete Account"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from 'src/core/users/infra/store/user.store';
+
+const userStore = useUserStore();
+let email = 'undefined';
+let userName = 'undefined';
+
+if (userStore.user !== null) {
+  email = userStore.user.email;
+  userName = userStore.user.userName;
+}
+</script>
+
+<style lang="scss" scoped>
+.q-separator--horizontal {
+  margin-inline: 11rem;
+}
+</style>
