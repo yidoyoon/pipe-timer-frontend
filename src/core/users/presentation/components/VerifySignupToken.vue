57eb155ed3584c728f6fd39b91b6af96eb27f65a
Yi Doyoon <yidoyoon@yidoyoon.com>
2023-06-01 04:15:47 +0100

    refactor(ux): modify condition for triggering notify
    Signed-off-by: Yi Doyoon <yidoyoon@yidoyoon.com>

--- src/core/users/presentation/components/VerifySignupToken.vue
+++ src/core/users/presentation/components/VerifySignupToken.vue
@@ -11,29 +11,32 @@ const $q = useQuasar();
 
 const userStore = useUserStore();
 
-onMounted(async () => {
+onBeforeMount(async () => {
   await router.isReady();
   const { signupToken } = route.query;
 
   try {
     const result = await verifyEmailFn(signupToken as string);
     if (result.success) {
+      if (userStore.user) {
+        const user = await getMeFn();
+        userStore.setUser(user);
+      }
+
+      await router.push({ name: 'panel' });
+
       $q.notify({
         type: 'positive',
         message: '이메일이 인증되었습니다.',
         icon: 'done',
       });
-
-      await router.push({ name: 'panel' });
     }
   } catch (err: any) {
-    if (err.response.data.message === 'Invalid signup token') {
-      $q.notify({
-        type: 'warning',
-        message: '이미 인증된 이메일이거나 유효하지 않은 토큰입니다.',
-        icon: 'warning',
-      });
-    }
+    $q.notify({
+      type: 'warning',
+      message: '이미 인증된 이메일이거나 유효하지 않은 토큰입니다.',
+      icon: 'warning',
+    });
 
     await router.push({ name: 'panel' });
   }
